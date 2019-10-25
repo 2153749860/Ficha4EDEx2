2ef8e6deccec59786469eecf3520fc65f895721b
2153749860 <tiagorocha1515@gmail.com>
2019-10-25 15:11:24 +0100

    metodo enqueue

--- src/ficha4ex2/CircularArrayQueue.java
+++ src/ficha4ex2/CircularArrayQueue.java
@@ -9,6 +9,68 @@ package ficha4ex2;
  *
  * @author tiago
  */
-public class CircularArrayQueue<T> implements StackADT2<T>{
+public class CircularArrayQueue<T> implements QueueADT<T>{
+    
+    private final int DEFAULT_CAPACITY = 10;
+    private int front;
+    private int rear;
+    private int count;
+    private T[] queue;
+
+    public CircularArrayQueue() {
+        front = 0;
+        rear = 0;
+        count = 0;
+        queue = (T[]) (new Object[DEFAULT_CAPACITY]);
+    }
+    
+    public CircularArrayQueue(int initialCapacity) {
+        front = 0;
+        rear = 0;
+        count = 0;
+        queue = (T[]) (new Object[initialCapacity]);
+    }
+
+    @Override
+    public void enqueue(T element) {
+        //v1
+        this.queue[this.rear] = element;
+        this.rear = (this.rear + 1) % this.queue.length;
+        this.count++;
+    }
+
+    @Override
+    public T dequeue() throws EmptyCollectionException2 {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T first() throws EmptyCollectionException2 {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+         return this.count;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("Front: ");str.append(this.queue[this.front]);str.append("|");str.append(this.front);str.append("\n");
+        str.append("Rear: ");str.append(this.queue[this.rear]);str.append("|");str.append(this.rear);str.append("\n");
+        str.append("Tamanho: ");str.append(this.size());str.append("\n");
+        str.append("Tamanho Stack: ");str.append(this.queue.length);str.append("\n");
+        for(int i=0; i<this.size(); i++){
+            str.append(this.queue[i]);str.append("\n");
+        }
+        return str.toString();
+    }
+
     
 }
--- src/ficha4ex2/Ficha4Ex2.java
+++ src/ficha4ex2/Ficha4Ex2.java
@@ -5,6 +5,9 @@
  */
 package ficha4ex2;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author tiago
@@ -16,6 +19,22 @@ public class Ficha4Ex2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        CircularArrayQueue lista = new CircularArrayQueue();
+
+        int numeroTeste = 1;
+
+        switch (numeroTeste) {
+            case 0: //teste adicionar 1 elemento
+               lista.enqueue(1);
+               System.out.println(lista.toString());
+               break;
+            case 1:
+                lista.enqueue(1);
+                lista.enqueue(2);
+                lista.enqueue(3);
+                System.out.println(lista.toString());
+                break;
+        }
     }
     
 }
